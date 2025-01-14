5f8293f4e2e014b3156696d57d6fc39003d11069
Jessica Plemmons <jessica.plemmons@hotmail.com>
2025-01-14 02:23:28 -0500

    updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let healthPoints = 20;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+} */
+
+/* let temperature = 20;
+
+if (temperature >= 30) {
+    console.log("It's hot outside.");
+} else if (temperature < 30 && temperature >= 15) {
+    console.log("The weather is moderate.");
+} else {
+    console.log("It's cold outside.");
+}*/
+
+/*
+let temperature = 5;
+
+if (temperature >= 30) {
+    console.log("It's hot outside.");
+} else if (temperature < 30 && temperature >= 15) {
+    console.log("The weather is moderate.");
+} else {
+    console.log("It's cold outside.");
+} */
+
+    
